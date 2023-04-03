aec76aa38de4f5fde8fdcdc6b4505313f76492d3
braedonwatkins <watkins.braedon@gmail.com>
2023-04-03 17:02:41 -0400

    remove unnecessary imports from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:cheapify/pages/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:cheapify/pages/login.dart';
 
 void main() {
   //Firebase.initializeApp(
