50a0242221dcdd35df95e5d27084a3ecda554f4b
Parker <pblume@Knights.ucf.edu>
2023-04-16 14:14:34 -0400

    Changed formatting and a few tweaks

--- lib/customComponents/custom_button.dart
+++ lib/customComponents/custom_button.dart
@@ -25,7 +25,7 @@ class CustomButton extends StatelessWidget
         ),
         child: Center(
           child: Text(
-            '$text',
+            text,
             style: TextStyle(
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
           ),
@@ -33,4 +33,61 @@ class CustomButton extends StatelessWidget
         ),
       );
     }
+}
+
+class SignInButton extends StatelessWidget
+{
+  final Function() ? onTap;
+  final String imageAsset;
+  final String text;
+
+  const SignInButton(
+    {
+      super.key,
+      required this.onTap,
+      required this.imageAsset,
+      required this.text,
+    });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      child: TextButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.grey.shade200),
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.zero)
+        ),
+        onPressed: onTap,
+        child: Padding(
+              padding: EdgeInsets.zero,
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset(imageAsset, height: 45),
+                          const SizedBox(width: 20),
+                          Text(
+                            text,
+                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+              ),
+        ),
+      ),
+   );
+  }    
 }
