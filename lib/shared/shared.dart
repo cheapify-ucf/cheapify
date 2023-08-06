6a00e6a260a1654e96d08f9239ea9cf4c25a7f03
braedonwatkins <watkins.braedon@gmail.com>
2023-08-05 20:47:32 -0400

    reorganizing

---
+++ lib/shared/shared.dart
@@ -0,0 +1,8 @@
+/* WHATS THIS???
+
+this is  a file used to export anything else that goes in the /shared directory
+
+so what's the /shared directory?
+well, it's just meant to be all the shared widgets that get used across many 
+pages. think like navbars or error modals or loading signs
+*/
--- lib/signup/signup.dart
+++ lib/signup/signup.dart
@@ -1,30 +1,28 @@
-import 'dart:ui';
+// import 'dart:ui';
 
-import 'package:cheapify/pages/welcome_screen.dart';
-import 'package:cheapify/customComponents/custom_TextFields.dart';
-import 'package:cheapify/customComponents/custom_button.dart';
+// import 'package:cheapify/pages/welcome_screen.dart';
+// import 'package:cheapify/customComponents/custom_TextFields.dart';
+// import 'package:cheapify/customComponents/custom_button.dart';
 import 'package:flutter/material.dart';
 
-class Signup extends StatelessWidget
-{
+class Signup extends StatelessWidget {
   Signup({super.key});
 
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-
   final _formKey = GlobalKey<FormState>();
 
-  void signUserIn()
-  {
-    if (_formKey.current!.validate()) {
-      print('valid');
-    } else {
-      print('Not valid');
-    }
+  void signUserIn() {
+    // if (_formKey.current!.validate()) {
+    //   print('valid');
+    // } else {
+    //   print('Not valid');
+    // }
   }
 
-  @override Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: SingleChildScrollView(
@@ -32,12 +30,10 @@ class Signup extends StatelessWidget
           height: MediaQuery.of(context).size.height,
           child: Stack(
             alignment: Alignment.center,
-            children: [
-              
-            ],
+            children: [],
           ),
         ),
       ),
-    ),
+    );
   }
 }
